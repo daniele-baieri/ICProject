e5debb024933f3fa626e930f127143cc1bb4aa92
attennig <46457883+attennig@users.noreply.github.com>
2021-06-10 18:39:28 +0200

    CUDA hello world

---
+++ MINLatinSquaresCUDA/hello.cu
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <cuda_runtime.h>
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
+__global__ void helloCUDA(float f) {
+    printf("hello thread %d, arg = %f", threadIdx.x, f);
+}
+
+int main() {
+    helloCUDA << <1, 5 >> > (0.4f);
+    cudaDeviceSynchronize();
+    return 0;
+}
